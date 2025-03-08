ca3b27c2ae72e48262783504f5f69a13689a350f
SHAKIB <shakibzero95@gmail.com>
2025-03-09 02:53:11 +0330

    change categories/[id] design

--- app/admin/categories/[id]/page.js
+++ app/admin/categories/[id]/page.js
@@ -1,21 +1,23 @@
 "use client";
-import Header from "@/app/components/ui/Header";
-import Sidebar from "@/app/components/ui/Sidebar";
+
+import Image from "next/image";
 import { useParams, useRouter } from "next/navigation";
 import React, { use, useEffect, useState } from "react";
-import { Alert, Button, Col, Container, Form, Row } from "react-bootstrap";
 
 const UpdateCategory = () => {
   const { id } = useParams();
   const [name, setName] = useState("");
   const [error, setError] = useState(null);
   const [formError, setFormError] = useState("");
+  const [loading, setLoading] = useState(true);
+
   const router = useRouter();
 
   useEffect(() => {
     fetch(`/api/categories/${id}`)
       .then((res) => res.json())
-      .then((data) => setName(data.name || ""));
+      .then((data) => setName(data.name || ""))
+      .then(() => setLoading(false));
   }, [id]);
 
   const validateForm = () => {
@@ -54,33 +56,82 @@ const UpdateCategory = () => {
     }
   };
   return (
-    <Container fluid>
-      <Row>
-        <Col md={2} className="vh-100">
-          <Sidebar />
-        </Col>
-        <Col md={10}>
-          <Header />
-          <main className="p-4">
-            <h2 className="my-4">ویرایش دسته بندی </h2>
-            {error && <Alert variant="danger">{error}</Alert>}
-            {formError && <Alert variant="warning">{formError}</Alert>}
+    // <Container fluid>
+    //   <Row>
+    //     <Col md={2} className="vh-100">
+    //       <Sidebar />
+    //     </Col>
+    //     <Col md={10}>
+    //       <Header />
+    //       <main className="p-4">
+    //         <h2 className="my-4">ویرایش دسته بندی </h2>
+    //         {error && <Alert variant="danger">{error}</Alert>}
+    //         {formError && <Alert variant="warning">{formError}</Alert>}
 
-            <Form onSubmit={handleSubmit}>
-              <Form.Group className="mb-3">
-                <Form.Label>نام دسته بندی</Form.Label>
-                <Form.Control
+    //         <Form onSubmit={handleSubmit}>
+    //           <Form.Group className="mb-3">
+    //             <Form.Label>نام دسته بندی</Form.Label>
+    //             <Form.Control
+    //               type="text"
+    //               value={name}
+    //               onChange={(e) => setName(e.target.value)}
+    //             />
+    //           </Form.Group>
+    //           <Button type="submit">ذخیره</Button>
+    //         </Form>
+    //       </main>
+    //     </Col>
+    //   </Row>
+    // </Container>
+    <div className="bg-shop-bg dark:bg-[#171a26] min-h-[100vh]">
+      <div className="relative h-[180px] min-h-[180px] w-full overflow-hidden rounded-b-xl">
+        <h1 className="text-white absolute z-10 right-8 top-6 font-bold text-3xl">ویرایش دسته بندی </h1>
+        <span className="text-white absolute z-10 right-8 top-20 text-xs sm:text-base">از این قسمت دسته بندی محصول را ویرایش کنید.</span>
+        <Image
+          className="absolute object-fill w-full h-full left-0 top-0 right-0 bottom-0 header-img"
+          src={"/uploads/top-header.png"}
+          alt="هدر"
+          width={1663}
+          height={277}
+        />
+      </div>
+      <div className="container py-4 px-10 -mt-10 z-50 relative">
+        <div className="bg-white py-4 px-4 rounded-lg shadow-xl shadow-[#112692]/5 dark:bg-shop-dark">
+          <div className="max-w-[400px] bg-white dark:bg-shop-dark">
+            {error && (
+              <h3 className="text-shop-red dark:text-gray-200 flex gap-x-2 items-center border border-shop-red/30 rounded py-1 px-2">
+                <svg className="dark:text-shop-red" width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M8 10.5378C8 9.43327 8.89543 8.53784 10 8.53784H11.3333C12.4379 8.53784 13.3333 9.43327 13.3333 10.5378V19.8285C13.3333 20.9331 14.2288 21.8285 15.3333 21.8285H16C16 21.8285 12.7624 23.323 10.6667 22.9361C10.1372 22.8384 9.52234 22.5913 9.01654 22.3553C8.37357 22.0553 8 21.3927 8 20.6832V10.5378Z"
+                    fill="currentColor"></path>
+                  <path
+                    d="M13 3.5C13 2.11929 11.8807 1 10.5 1C9.11929 1 8 2.11929 8 3.5C8 4.88071 9.11929 6 10.5 6C11.8807 6 13 4.88071 13 3.5Z"
+                    fill="currentColor"></path>
+                </svg>
+                {error}
+              </h3>
+            )}
+            {formError && <h3>{formError}</h3>}
+            <form className="py-4" onSubmit={handleSubmit}>
+              <div className="flex flex-col items-start gap-y-4 w-full">
+                <input
+                  name="name"
+                  className="focus:outline-none border dark:bg-shop-dark dark:border-gray-600 dark:text-gray-200 dark:placeholder:text-gray-200 border-gray-200 rounded px-4 py-2 w-full focus:ring-2 focus:ring-shop-red transition-all duration-300"
+                  placeholder={`${loading ? "در حال بارگذاری..." : ""}`}
                   type="text"
                   value={name}
                   onChange={(e) => setName(e.target.value)}
+                  disabled={loading}
                 />
-              </Form.Group>
-              <Button type="submit">ذخیره</Button>
-            </Form>
-          </main>
-        </Col>
-      </Row>
-    </Container>
+                <button type="submit" className="bg-green-500 text-white py-2 px-4 rounded">
+                  ذخیره
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
