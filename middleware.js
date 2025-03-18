7e05faabb072dd0a7945a9bb84416225a5a1aba4
SHAKIB <shakibzero95@gmail.com>
2025-03-18 15:21:10 +0330

    Create middleware to check isAdmin

---
+++ middleware.js
@@ -0,0 +1,18 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/auth/login", req.url));
+  }
+  if (!token.isAdmin) {
+    return NextResponse.redirect(new URL("/unauthorize", req.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin/:path*"],
+};
