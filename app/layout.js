a8b1240b8153a0725cb60793450e88564a842ae3
SHAKIB <shakibzero95@gmail.com>
2025-03-28 18:31:53 +0330

    delete recaptcha script from layout.js and add it to LoginForm component.add loadRecaptchaToken function to LoginForm component

--- app/layout.js
+++ app/layout.js
@@ -16,10 +16,6 @@ export default function RootLayout({ children }) {
         <SessionProviderWrapper>
           <ThemeProvider>{children}</ThemeProvider>
         </SessionProviderWrapper>
-        <Script
-          src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
-          strategy="afterInteractive"
-        />
       </body>
     </html>
   );
