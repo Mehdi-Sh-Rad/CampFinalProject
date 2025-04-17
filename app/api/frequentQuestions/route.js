5141b8ff6f7b6c08b4c3c84e636e981725dad2c6
SHAKIB <shakibzero95@gmail.com>
2025-04-17 22:16:29 +0330

    Add validation to frequentQuestions API

--- app/api/frequentQuestions/route.js
+++ app/api/frequentQuestions/route.js
@@ -12,42 +12,43 @@ export async function POST(request) {
   try {
     const body = await request.json();
 
-    if (
-      !body.question ||
-      typeof body.question !== "string" ||
-      body.question.trim() === ""
-    ) {
-      return new Response(
-        JSON.stringify({ message: "نام دسته بندی الزامی میباشد" }),
-        {
-          status: 400,
-        }
-      );
+    // Validate topic
+    if (!body.topic || typeof body.topic !== "string" || body.topic.trim() === "") {
+      return new Response(JSON.stringify({ message: "موضوع الزامی است" }), {
+        status: 400,
+      });
     }
-    if (body.topic.length < 1 || body.topic.length > 50) {
-      return new Response(
-        JSON.stringify({ message: "موضوع سوال باید بین ۱ تا ۵۰ کاراکتر باشد" }),
-        {
-          status: 400,
-        }
-      );
+    if (body.topic.length < 3 || body.topic.length > 50) {
+      return new Response(JSON.stringify({ message: "موضوع باید بین ۳ تا ۵۰ کاراکتر باشد" }), {
+        status: 400,
+      });
     }
-    if (body.question.length < 1 || body.question.length > 100) {
-      return new Response(
-        JSON.stringify({ message: "سوال باید بین ۱ تا ۱۰۰ کاراکتر باشد" }),
-        {
-          status: 400,
-        }
-      );
+
+    // Validate question
+    if (!body.question || typeof body.question !== "string" || body.question.trim() === "") {
+      return new Response(JSON.stringify({ message: "سوال الزامی است" }), {
+        status: 400,
+      });
     }
-    if (body.answer.length < 1 || body.answer.length > 300) {
-      return new Response(
-        JSON.stringify({ message: "پاسخ باید بین ۱ تا ۳۰۰ کاراکتر باشد" }),
-        {
-          status: 400,
-        }
-      );
+    if (body.question.length < 3 || body.question.length > 100) {
+      return new Response(JSON.stringify({ message: "سوال باید بین ۳ تا ۱۰۰ کاراکتر باشد" }), {
+        status: 400,
+      });
     }
+
+    // Validate answer
+    if (!body.answer || typeof body.answer !== "string" || body.answer.trim() === "") {
+      return new Response(JSON.stringify({ message: "پاسخ الزامی است" }), {
+        status: 400,
+      });
+    }
+    if (body.answer.length < 3 || body.answer.length > 500) {
+      return new Response(JSON.stringify({ message: "پاسخ باید بین ۳ تا ۵۰۰ کاراکتر باشد" }), {
+        status: 400,
+      });
+    }
+
+    // Create frequent question
     const frequentQues = await FrequentQuestion.create(body);
     return new Response(JSON.stringify(frequentQues), { status: 200 });
   } catch (error) {
